ec2d80af1f86ff94a87f3fd256f446ab3276f611
M. Agung Wibowo <111426359+bwxids@users.noreply.github.com>
2023-06-11 02:07:56 +0700

    add image

--- projects/script.js
+++ projects/script.js
@@ -30,14 +30,14 @@ document.addEventListener('visibilitychange',
     });
 
 
-// fetch projects start
-// function getProjects() {
-//     return fetch("projects.json")
-//         .then(response => response.json())
-//         .then(data => {
-//             return data
-//         });
-// }
+fetch projects start
+function getProjects() {
+    return fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        });
+}
 
 
 function showProjects(projects) {
